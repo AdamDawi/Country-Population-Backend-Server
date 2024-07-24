48db02a937e3b96c5ef1f336cc30e91e4be7f66c
Adam Dawidziuk <adamdawidziuk@onet.pl>
2024-07-24 18:39:00 +0200

    UPDATE: deprecated testing API to newest in 2.0.0 ktor

--- src/test/kotlin/example/com/ApplicationTest.kt
+++ src/test/kotlin/example/com/ApplicationTest.kt
@@ -1,146 +1,115 @@
 package example.com
 
-import CountryRepository
 import example.com.common.Constants.PAGE_NOT_FOUND_MESSAGE
 import example.com.common.Constants.WELCOME_MESSAGE
 import example.com.models.ApiResponse
 import example.com.models.Country
+import example.com.repository.CountryRepositoryImpl
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.application.*
 import io.ktor.server.testing.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import org.koin.java.KoinJavaComponent.inject
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
-    private val countryRepository: CountryRepository by inject(CountryRepository::class.java)
-
     @Test
-    fun root_accessEndpoint_correctMessageResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(WELCOME_MESSAGE, response.content)
-            }
-        }
+    fun root_accessEndpoint_correctMessageResponse() = testApplication {
+        val response = client.get("/")
+
+        assertEquals(WELCOME_MESSAGE, response.bodyAsText())
     }
     @Test
-    fun root_accessEndpoint_OKStatusResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-            }
-        }
+    fun root_accessEndpoint_OKStatusResponse() = testApplication{
+        val response = client.get("/")
+
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 
     @Test
-    fun getAllCountries_accessEndpoint_correctCountriesListResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries").apply {
-                val response = Json.decodeFromString<ApiResponse>(response.content.toString())
-                assertEquals(countryRepository.countryList, response.countries)
-            }
-        }
+    fun getAllCountries_accessEndpoint_correctCountriesListResponse() = testApplication{
+        val countryRepository = CountryRepositoryImpl()
+
+        val response = Json.decodeFromString<ApiResponse>(client.get("/countries").bodyAsText())
+
+        assertEquals(countryRepository.countryList, response.countries)
     }
     @Test
-    fun getAllCountries_accessEndpoint_OKStatusResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-            }
-        }
+    fun getAllCountries_accessEndpoint_OKStatusResponse() = testApplication{
+        val response = client.get("/countries")
+
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 
     @Test
-    fun nonExistingEndpoint_accessEndpoint_correctMessageResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/dsdsa").apply {
-                assertEquals(PAGE_NOT_FOUND_MESSAGE, response.content)
-            }
-        }
+    fun nonExistingEndpoint_accessEndpoint_correctMessageResponse() = testApplication{
+        val response = client.get("/dsdsa")
+
+        assertEquals(PAGE_NOT_FOUND_MESSAGE, response.bodyAsText())
     }
 
     @Test
-    fun nonExistingEndpoint_accessEndpoint_NotFoundStatusResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/dsdsa").apply {
-                assertEquals(HttpStatusCode.NotFound, response.status())
-            }
-        }
+    fun nonExistingEndpoint_accessEndpoint_NotFoundStatusResponse() = testApplication{
+        val response = client.get("/dsdsa")
+
+        assertEquals(HttpStatusCode.NotFound, response.status)
     }
 
     @Test
-    fun searchCountries_accessEndpointWithoutParameter_emptyCountryListResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries/search").apply {
-                val response = Json.decodeFromString<ApiResponse>(response.content.toString())
-                assertEquals(emptyList(), response.countries)
-            }
-        }
+    fun searchCountries_accessEndpointWithoutParameter_emptyCountryListResponse() = testApplication{
+        val response = Json.decodeFromString<ApiResponse>(client.get("/countries/search").bodyAsText())
+
+        assertEquals(emptyList(), response.countries)
     }
 
     @Test
-    fun searchCountries_accessEndpointWithoutParameter_BadRequestStatusResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries/search").apply {
-                assertEquals(HttpStatusCode.BadRequest, response.status())
-            }
-        }
+    fun searchCountries_accessEndpointWithoutParameter_BadRequestStatusResponse() = testApplication{
+        val response = client.get("/countries/search")
+
+        assertEquals(HttpStatusCode.BadRequest, response.status)
     }
 
     @Test
-    fun searchCountries_accessEndpointWithNumberParameter_emptyCountryListResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries/search?name=21").apply {
-                val response = Json.decodeFromString<ApiResponse>(response.content.toString())
-                assertEquals(emptyList(), response.countries)
-            }
-        }
+    fun searchCountries_accessEndpointWithNumberParameter_emptyCountryListResponse() = testApplication{
+        val response = Json.decodeFromString<ApiResponse>(client.get("/countries/search?name=21").bodyAsText())
+
+        assertEquals(emptyList(), response.countries)
     }
 
     @Test
-    fun searchCountries_accessEndpointWithNumberParameter_BadRequestStatusResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries/search?name=21").apply {
-                assertEquals(HttpStatusCode.BadRequest, response.status())
-            }
-        }
+    fun searchCountries_accessEndpointWithNumberParameter_BadRequestStatusResponse() = testApplication{
+        val response = client.get("/countries/search?name=21")
+
+        assertEquals(HttpStatusCode.BadRequest, response.status)
     }
 
     @Test
-    fun searchCountries_accessEndpointWithParameter_correctSingleCountryResponse() {
+    fun searchCountries_accessEndpointWithParameter_correctSingleCountryResponse() = testApplication{
         val expected = listOf(
             Country("North Korea", 25778816, "/images/KP.png"),
             Country("South Korea", 51269185, "/images/KR.png")
         )
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries/search?name=kor").apply {
-                val response = Json.decodeFromString<ApiResponse>(response.content.toString())
-                assertEquals(expected, response.countries)
-            }
-        }
+        val response = Json.decodeFromString<ApiResponse>(client.get("/countries/search?name=kor").bodyAsText())
+
+        assertEquals(expected, response.countries)
     }
 
     @Test
-    fun searchCountries_accessEndpointWithParameter_correctMultipleCountryResponse() {
+    fun searchCountries_accessEndpointWithParameter_correctMultipleCountryResponse() = testApplication{
         val expected = listOf(Country("Poland", 38386000, "/images/PL.png"))
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries/search?name=pol").apply {
-                val response = Json.decodeFromString<ApiResponse>(response.content.toString())
-                assertEquals(expected, response.countries)
-            }
-        }
+        val response = Json.decodeFromString<ApiResponse>(client.get("/countries/search?name=pol").bodyAsText())
+
+        assertEquals(expected, response.countries)
     }
 
     @Test
-    fun searchCountries_accessEndpointWithParameter_OKStatusResponse() {
-        withTestApplication(moduleFunction = Application::module) {
-            handleRequest(HttpMethod.Get, "/countries/search?name=kor").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-            }
-        }
+    fun searchCountries_accessEndpointWithParameter_OKStatusResponse() = testApplication{
+        val response = client.get("/countries/search?name=kor")
+
+        assertEquals(HttpStatusCode.OK, response.status)
     }
 
 
